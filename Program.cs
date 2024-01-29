8b1eea213d1ddc13f3c110445f93363b236777c8
magalyrodriguez1 <142467680+magalyrodriguez1@users.noreply.github.com>
2024-01-28 16:07:50 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Author: Magaly Rodriguez
+ * Course: COMP-003A
+ * Purpose: Learning programming basics like strings and doing calculations
+ * */
+
+
+namespace COMPA003A.Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "COMP-003A - Assignment 2";
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            string firstName = Console.ReadLine();
+            string middleName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            string inputAge = Console.ReadLine();
+            int yearBorn = 2023 - Convert.ToInt32(inputAge);
+            Console.WriteLine($"Hello, {firstName} {middleName} {lastName}. You were born in {yearBorn}.");
+            
+
+
+        }
+    }
+}
